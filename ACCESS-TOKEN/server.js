65a4f10b6f6b06e9351b1f3917523cb4abb31c8c
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-07 15:46:11 +0530

    post is set

--- ACCESS-TOKEN/server.js
+++ ACCESS-TOKEN/server.js
@@ -1,10 +1,13 @@
+
+require('dotenv').config()
 const express = require ("express")
 const app= express()
 const jwt= require("jsonwebtoken")
+const { restart } = require("nodemon")
 
 app.use(express.json())
-const posts=[
-    {
+const posts=[ 
+    { 
 
     username:'Novabella',
     title:'post 1'
@@ -29,10 +32,11 @@ app.get('/posts',(req,res)=>{
 
  app.post('/login',(req,res)=>{
    //Authenticate User
-   const username = req.body.username
-   const user= {name:username}
-   jwt .sign(user,process.env.ACCESS_TOKEN_SECRET)
-
+//    res.send('hai')
+    const username = req.body.username
+    const user= {name:username}
+    const accessToken= jwt .sign(user,process.env.ACCESS_TOKEN_SECRET)
+     res.json({accessToken:accessToken})
  }) 
 
 
