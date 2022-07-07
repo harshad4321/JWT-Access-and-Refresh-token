317a1462bc2ba6e56535f3172c4a62082be03010
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-07 16:38:37 +0530

    fix the bug

--- ACCESS-TOKEN/server.js
+++ ACCESS-TOKEN/server.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 const express = require ("express")
 const app= express()
 const jwt= require("jsonwebtoken")
-const { restart } = require("nodemon")
+
 
 app.use(express.json())
-const posts=[ 
+const posts=[  
     { 
 
-    username:'Novabella',
+    username:'Nova',
     title:'post 1'
 },
 {
@@ -19,26 +19,36 @@ const posts=[
 },
 {
 
-    username:'Nova',
+    username:'Nova2',
     title:'post 3'
 },
 
 
 ]
 
-app.get('/posts',(req,res)=>{
-    res.json(posts)
+app.get('/posts',AuthenticateToken,(req,res)=>{
+    res.json(posts.filter(post=>post.username===req.user.name))
 })
-
- app.post('/login',(req,res)=>{
-   //Authenticate User
-//    res.send('hai')
+ 
+ app.post('/login',(req,res)=>{ 
+   //Authenticate User 
+//    res.send('hai')  
     const username = req.body.username
     const user= {name:username}
     const accessToken= jwt .sign(user,process.env.ACCESS_TOKEN_SECRET)
      res.json({accessToken:accessToken})
  }) 
+function AuthenticateToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+   if (token==null)return res.sendStatus(401)
 
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,user)=>{
+    if(err)return res.sendStatus(403).json('something rong')
+    req.user=user
+    next()
+   })
+}
 
 
 
