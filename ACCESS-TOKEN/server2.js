4ff556cc3b426db8115f06ac8e738940f865e558
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-07 18:08:58 +0530

    cross multiple server it workes

--- ACCESS-TOKEN/server2.js
+++ ACCESS-TOKEN/server2.js
@@ -1,4 +1,4 @@
-
+ 
 require('dotenv').config()
 const express = require ("express")
 const app= express()
@@ -52,6 +52,6 @@ function AuthenticateToken(req,res,next){
 
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, console.log(`Listening on port ${PORT}!!!!.`)); 
