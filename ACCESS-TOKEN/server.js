39ac78f35e290a94d25ed0bb691eaf80944dfa1b
Harshad N <103301901+harshad4321@users.noreply.github.com>
2022-07-07 15:23:56 +0530

    get & post & username set

--- ACCESS-TOKEN/server.js
+++ ACCESS-TOKEN/server.js
@@ -2,6 +2,40 @@ const express = require ("express")
 const app= express()
 const jwt= require("jsonwebtoken")
 
+app.use(express.json())
+const posts=[
+    {
+
+    username:'Novabella',
+    title:'post 1'
+},
+{
+
+    username:'john',
+    title:'post 2'
+},
+{
+
+    username:'Nova',
+    title:'post 3'
+},
+
+
+]
+
+app.get('/posts',(req,res)=>{
+    res.json(posts)
+})
+
+ app.post('/login',(req,res)=>{
+   //Authenticate User
+   const username = req.body.username
+   const user= {name:username}
+   jwt .sign(user,process.env.ACCESS_TOKEN_SECRET)
+
+ }) 
+
+
 
 
 const PORT = process.env.PORT || 3000;
